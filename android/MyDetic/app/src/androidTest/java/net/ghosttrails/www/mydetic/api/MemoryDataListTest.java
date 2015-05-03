af0b7faa55939c81b9e06666ac1ee0d7c008c3bd
Andrew Reid <andrew.k.reid@gmail.com>
2015-05-03 11:49:56 +1000

    Finish MemorydataList tests

--- android/MyDetic/app/src/androidTest/java/net/ghosttrails/www/mydetic/api/MemoryDataListTest.java
+++ android/MyDetic/app/src/androidTest/java/net/ghosttrails/www/mydetic/api/MemoryDataListTest.java
@@ -8,11 +8,11 @@ import java.util.Iterator;
 import java.util.Set;
 
 /**
- * Created by andrewr on 29/04/15.
+ * Tests for MemoryDataList
  */
 public class MemoryDataListTest extends TestCase {
 
-    public void testOrdering() {
+    public void testEverything() {
         MemoryDataList list = new MemoryDataList();
         assertEquals(0, list.getDates().size());
 
@@ -23,6 +23,10 @@ public class MemoryDataListTest extends TestCase {
         assertEquals(2, list.getDates().size());
         Iterator<Date> dates = list.getDates().iterator();
 
+        assertTrue(list.hasDate(new GregorianCalendar(2010, 10, 23).getTime()));
+        assertFalse(list.hasDate(new GregorianCalendar(2012, 9, 17).getTime()));
+
+        // should be in ascending date order.
         assertTrue(dates.hasNext());
         assertEquals(new GregorianCalendar(2010, 10, 23).getTime(), dates.next());
         assertEquals(new GregorianCalendar(2010, 11, 23).getTime(), dates.next());
